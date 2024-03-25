9c8a224f0eb2a8b5b2fa3bf3fde22c27ec590822
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-03-25 13:36:07 +0200

    First init

---
+++ First/app/routes/_index.tsx
@@ -0,0 +1,27 @@
+// import type { MetaFunction } from "@remix-run/node";
+
+import { Link } from "@remix-run/react";
+import home from '~/styles/home.css'
+
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: "New Remix App" },
+//     { name: "description", content: "Welcome to Remix!" },
+//   ];
+// };
+
+export default function Index() {
+  return (
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
+  );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: home }]
+}
